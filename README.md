5d378d217b5f7c1da2f9d6b9baf08ffc214ac60c
Nicolas Lang <git@nicolas-lang.de>
2021-06-12 22:28:13 +0200

    Initial mod commit

--- README.md
+++ README.md
@@ -1,22 +1,16 @@
-# Factorio.ModTemplate
-Template for automated mod deployment using Github Actions when a tagged commit is pushed
+# Factorio: Space Shuttle
+When playing Factorio with Earendel's Space Exploration mod, I never understood why there only is spacewalking in orbit.
 
-## Inputs (Github secrets)
+This mod adds a space shuttle (based on the cargo plane) when the mod aircraft is installed.
 
-### `factorio_user`
+Feel free to review and improve my code.
 
-**Required** User that will be used to authenticate to the Factorio mod-portal.
-
-### `factorio_password`
-
-**Required** Password that will be used to authenticate to the Factorio mod-portal.
+## Known issues
+- The models look very bad: yes they do, feel free to make a pull request with improved gfx.
+- There might be issues when adding the mod to an existing game
 
 ## Acknowledgements
 
-Actions based on:
-- [Roang-zero1 Actions](https://github.com/Roang-zero1)
-
-Which are  based on:
-- [Nexelas Mods](https://github.com/Nexela)
-- [GitHub Action to automatically publish to the Factorio mod portal](https://github.com/shanemadden/factorio-mod-portal-publish)
-- Shane Madden (Nymbia)
+- [Github actions based on Roang-zero1 Actions](https://github.com/Roang-zero1)
+- [Space Exploration is a mod by Earendel](https://mods.factorio.com/mod/space-exploration)
+- [Aircraft is a mod by SuicidalKid](https://mods.factorio.com/mod/Aircraft)
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,1 +1,44 @@
-log("data-final-fixes")
+﻿local data = _G.data
+local mods = _G.mods
+----------------------------------------------------------------------------------------------------------------- 		
+local function has_value (tab, val)
+	for index, value in ipairs(tab) do
+		if value == val then
+			return true
+		end
+	end
+	return false
+end
+--===================================================================================================
+-- If we are using Aircrafts and SE give us space shuttles
+--===================================================================================================
+if mods["Aircraft"] then
+----------------------------------------------------------------------------------------------------------------- 		
+	if mods["space-exploration"] then
+		local entity = util.table.deepcopy(data.raw["car"]["cargo-plane"])
+		entity["animation"]["filename"] = "__nco-SpaceShuttle__/graphics/se-space-shuttle_spritesheet_shadowless.png"
+		entity.name = "se-space-shuttle"
+		entity.minable = {mining_time = 1, result = "se-space-shuttle"}
+		local item = util.table.deepcopy(data.raw["item-with-entity-data"]["cargo-plane"])
+		item.name = "se-space-shuttle"
+		item.icon = "__nco-SpaceShuttle__/graphics/se-space-shuttle_icon.png"
+		item.place_result = "se-space-shuttle"
+		local recipe = util.table.deepcopy(data.raw["recipe"]["cargo-plane"])
+		recipe.name = "se-space-shuttle"
+		recipe.result = "se-space-shuttle"
+		recipe.ingredients = {{"cargo-plane",1},{"se-lifesupport-equipment-1",5},{"se-heat-shielding",1000}}
+		recipe.expensive.result = "se-space-shuttle"
+		recipe.expensive.ingredients = recipe.ingredients
+		data:extend({entity})
+		data:extend({item})
+		data:extend({recipe})
+		table.insert(data.raw["technology"]["cargo-planes"]["effects"], {type = "unlock-recipe",recipe = "se-space-shuttle"})
+	end
+-----------------------------------------------------------------------------------------------------------------
+	local planes = {"gunship","cargo-plane","jet","flying-fortress","cargo-plane-green","se-space-shuttle"}
+	for _ ,rec in pairs(data.raw["recipe"]) do
+		if has_value(planes, rec.name) then
+			rec.subgroup = "airplanes"
+		end
+	end
+end
